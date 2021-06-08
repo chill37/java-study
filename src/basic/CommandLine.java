2a367d3b11e5dad2d305e4cf0374061d8f6969d0
김태우 책임연구원/블록체인플랫폼팀 <tkim@lgcns.com>
2021-06-08 17:05:53 +0900

    process builder change

--- src/basic/CommandLine.java
+++ src/basic/CommandLine.java
@@ -1,9 +1,11 @@
 package basic;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -14,6 +16,60 @@ public class CommandLine {
 		String ans = printTasklist("ls", "-l", "./TEXT/");
 		System.out.println("[ANS]" + ans);
 		
+		printTasklist2("ls", "-l", "./TEXT/");
+		//printTasklist3();
+		
+	}
+	
+	public static void printTasklist3() throws IOException {
+		String[] cmd = new String[] {"cmd", "/c", "dir"};
+		Process process = null;
+		String str = null;
+		
+		try {
+			process = new ProcessBuilder(cmd).start();
+			BufferedReader stdOut = new BufferedReader(new InputStreamReader(process.getInputStream()));
+			
+			while((str = stdOut.readLine()) != null) {
+				System.out.println(str);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void printTasklist2(String... args) {
+		String[] commands = new String[] {
+				"ping -n 5 127.0.0.1"
+			};
+
+			try {
+				ProcessBuilder b = new ProcessBuilder("bash");
+				b.redirectErrorStream(true);
+				Process p = b.start();
+
+				BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
+
+				for (String cmd : commands) {
+					writer.write(cmd + "\n");
+					writer.flush();
+				}
+
+				writer.write("exit" + "\n");
+				writer.flush();
+
+				BufferedReader std = new BufferedReader(new InputStreamReader(p.getInputStream()));
+				String outputLine = "";
+				String outputMessage = "";
+				while ((outputLine = std.readLine()) != null) {
+					outputMessage += outputLine + "\r\n";
+				}
+
+				p.waitFor();
+				
+				System.out.println(outputMessage);
+			} 
+			catch (Exception e) { e.printStackTrace(); }
 	}
 	
 	public static String printTasklist(String exeCmd, String param, String folder) {
